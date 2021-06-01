c2f7c9648a2b185314664a88565774007b562c7d
moraleseoscar <oestrada.3@gmail.com>
2021-05-31 22:10:19 -0600

    Vista de login y register hechas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'registro', component: RegisterComponent},
+  {path:'home', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
